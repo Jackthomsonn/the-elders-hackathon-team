edccfb6adcbec789b7a17ba839aac9d3ddf586d6
Jack Thomson <jackthomson@Jacks-MacBook-Pro.local>
2022-09-08 13:47:00 +0100

    deploy

--- components/tips.tsx
+++ components/tips.tsx
@@ -11,8 +11,8 @@ export function Tips ({ tips,tipUpvotes }: IProps) {
   return <div>
     <h3 className={styles.tipsTitle}>Here are some helpful tips to save energy!</h3>
     <ul>
-      {tips.map(tip => {
-        return ( <TipEntity tipKey={tip.key} content={tip.content} upvotes={tipUpvotes}/>
+      {tips.map((tip, index) => {
+        return ( <TipEntity key={index} tipKey={tip.key} content={tip.content} upvotes={tipUpvotes}/>
         )
       })}
       </ul>
